58aca914950f0df0804377e776dec31086f74868
Aryan <chaudharyaryan3456@gmail.com>
2025-02-05 13:44:56 +0530

    updated

--- app.js
+++ app.js
@@ -2,13 +2,59 @@
 //Why is there a need to use Axios when we already have fetch() method? 
 // It is because when we used fetch(), it sends the response in a readable stream and not in the direct json form. And because of that, we first had to parse that data into the json format. But inside Axios, the response is directly in the json format and we don't have to parse the data first. So, it is a better way to access our data directly. 
 
-let url = "https://catfact.ninja/fact";
+// let url = "https://catfact.ninja/fact";
 
-async function getfacts() {
+// async function getfacts() {
+//     try{
+//         let res = await axios.get(url);
+//         console.log(res.data);
+//     }catch(e){
+//         console.log("error -", e);
+//     }
+// }
+
+// CODE TO PRINT CATFACTS INSIDE PARAGRAPH TAG THROUGH DOM. Always 
+// *** remember that we are using free APIs here, and all these APIs have some callback limits, so don't try to call the APIs too many times or don't run a loop for them because if we do so then our account can be blocked and we will also get some error at last. 
+
+
+let h1 = document.querySelector("h1");
+let btn = document.querySelector("button");
+let para = document.getElementById("fact");
+let Img = document.querySelector("img");
+
+// let url = "https://catfact.ninja/fact";
+
+// btn.addEventListener("click", async function () {
+
+//     try{
+//         let res = await axios.get(url);
+//         para.innerText = res.data.fact;
+//         para.style.fontSize = '50px';
+//         para.style.color = "red";
+//     }catch(e){
+//         console.log("error -", e);
+//     }
+
+// });
+
+
+// CODE TO PRINT DOG IMAGES INSIDE IMG TAG THROUGH DOM. 
+// *** Always remember that we are using free APIs here, and all these APIs have some callback limits, so don't try to call the APIs too many times or don't run a loop for them because if we do so then our account can be blocked and we will also get some error at last. 
+
+
+let url2 = "https://dog.ceo/api/breeds/image/random";
+
+async function getImage() {
     try{
-        let res = await axios.get(url);
-        console.log(res.data);
+        let res = await axios.get(url2);
+        return res.data.message;
     }catch(e){
-        console.log("error -", e);
+        console.log("Error -", e);
+        return "NO Image";
     }
 }
+
+btn.addEventListener("click", async () => {
+    let Imag = await getImage();
+    Img.src = Imag;
+});
